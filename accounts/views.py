2d9a1e58983c74da118b013fdc6ddf53428644c2
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2022-07-19 23:08:34 +0545

    account  setup process

--- accounts/views.py
+++ accounts/views.py
@@ -1,43 +1,81 @@
-from django.shortcuts import  render, redirect
+from django.shortcuts import render, redirect
+from accounts.models import Account
 from .forms import NewUserForm, CustomAuthForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.http.response import JsonResponse
+from rest_framework import status
+import requests
 
 def homepage(request):
-	return render(request=request, template_name="index.html")
+    return render(request=request, template_name="index.html")
+
 
 def register(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("authentication:login")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request=request, template_name="register.html", context={"register_form":form})
+    if request.method == "POST":
+        context = {}
+        full_name = request.POST['full_name']
+        email = request.POST['email']
+        role = request.POST['role']
+        phone = request.POST['phone']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password != confirm_password:
+            context['message'] = 'Password and Confirm Password Do Not Match'
+            context['code'] = status.HTTP_400_BAD_REQUEST
+            return JsonResponse(context, status=200)
+        
+        if Account.objects.filter(email=email).exists():
+            context['message'] = 'Email Already Exists'
+            context['code'] = status.HTTP_400_BAD_REQUEST
+            return JsonResponse(context, status=200)
+        
+        else:
+            user = Account.objects.create_user(
+                full_name=full_name, email=email, password=password, role=role, phone=phone)
+            messages.success(request, 'Thank you for registering with us')
+            context={'message':'Thank you for registering with us.','code':status.HTTP_200_OK}
+            return JsonResponse(context, status=201)
 
 def login_request(request):
-	if request.method == "POST":
-		form = CustomAuthForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("authentication:homepage")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = CustomAuthForm()
-	return render(request=request, template_name="login.html", context={"login_form":form})
+    if request.method == "POST":
+        context={}
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(email=email, password=password)
+        
+        if user is None:
+            context['message1'] = 'Invalid Login Details!'
+            context['message2'] = 'Please Try Again With A Valid Email And Password'
+            context['code'] = status.HTTP_401_UNAUTHORIZED
+            return JsonResponse(context, status=200)
+        
+        elif user is not None:
+            login(request, user)
+            context['message'] = 'Successfully authenticated.'
+            context['status'] = 'Success!'
+            context['code'] = status.HTTP_200_OK
+            messages.success(request, 'You are now logged in.')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split("=") for x in query.split('&'))
+                if 'next' in params:
+                    nextpage = params['next']
+                    return redirect(nextpage)     
+
+            except:
+                return JsonResponse(context, status=200)        
+        else:
+            context['message'] = 'Invalid credentials'
+            context['message2'] = 'Kindly Try Again With A Valid Email And Password'
+            context['code'] = status.HTTP_401_UNAUTHORIZED
+            return JsonResponse(context, status=400)
+    return JsonResponse({'action':'Not allowed'}, status=400)
 
 def logout_request(request):
-	logout(request)
-	messages.info(request, "You have successfully logged out.") 
-	return redirect("authentication:homepage")
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("authentication:homepage")

545624b88b8ad9c7c78bc289bce6aa9f8934d36f
Barsha11 <barshamaharjan8@gmail.com>
2022-08-16 12:16:07 +0545

    degin fixes of alert and chat

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -91,3 +91,29 @@ $('.courses-slider').owlCarousel({
         $(this).remove(); 
     });
 }, 4000);
+
+// logging key-value pairs of FormData object
+function logFormData(data) {
+    for (const pair of data.entries()) {
+        console.log(pair[0] + ": " + pair[1]);
+    }
+}
+
+
+// fade out message alerts
+function fade_alerts() {
+    alerts = document.getElementsByClassName("alert msg");
+        var i = alerts.length;
+        for (let elem of alerts) {
+            i--;
+            time = 3250+(1000*i);
+            setTimeout(function() {
+                $(elem).fadeOut("slow");
+            }, time);
+        }
+}
+
+// call fade out after DOMContentLoaded
+window.addEventListener('DOMContentLoaded', (event) => {
+    fade_alerts();
+});
